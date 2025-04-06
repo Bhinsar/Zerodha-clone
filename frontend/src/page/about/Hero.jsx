48a93c4554f5d1c2978c009a5b9545f07a1bfb28
Bhinsar <bhinsarjagdish2003@gmail.com>
2025-04-06 12:46:52 +0530

    Wrap Hero component content in a fragment for improved structure and potential future enhancements.

--- frontend/src/page/about/Hero.jsx
+++ frontend/src/page/about/Hero.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <div className="flex justify-center items-center px-28 py-28 text-3xl text-gray-800/90 font-[500]">
-      We pioneered the discount broking model in India.
-      <br />
-      Now, we are breaking ground with our technology.
-    </div>
+    <>
+      <div className="flex justify-center items-center px-28 py-28 text-3xl text-gray-800/90 font-[500]">
+        We pioneered the discount broking model in India.
+        <br />
+        Now, we are breaking ground with our technology.
+      </div>
+    </>
   );
 }
