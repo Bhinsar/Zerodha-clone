19e53938433189410691feca92d0a8aad3f0047f
Bhinsar <bhinsarjagdish2003@gmail.com>
2025-04-07 13:13:34 +0530

    Add ProductPage and related components: Hero, LeftImage, and Universe. Update routing in App.jsx to include ProductPage. Enhance layout and content for better user engagement.

--- frontend/src/page/products/LeftImage.jsx
+++ frontend/src/page/products/LeftImage.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function LeftImage() {
+export default function LeftImage({
+  image,
+  title,
+  content,
+  tryDemo,
+  learnMore,
+  playStore,
+  appleStore,
+}) {
   return (
-    <div>LeftImage</div>
-  )
+    <div className="grid grid-cols-1 lg:grid-cols-3 justify-between items-center lg:gap-8 py-20 px-4">
+      <div className="col-span-2">
+        <Link to="/home">
+          <img src={image} />
+        </Link>
+      </div>
+      <div>
+        <div className="text-[2rem] mt-11 text-gray-700 font-[500]">
+          {title}
+        </div>
+        <p className="text-[1rem] text-gray-600 ">{content}</p>
+      </div>
+    </div>
+  );
 }
--- frontend/src/page/products/ProductPage.jsx
+++ frontend/src/page/products/ProductPage.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import Hero from './Hero'
+import React from "react";
+import Hero from "./Hero";
+import LeftImage from "./LeftImage";
+import RightImage from "./RightImage";
+import Universe from "./Universe";
 
 export default function ProductPage() {
+  const LeftImageContent = [
+    {
+      image: "/assets/kite.png",
+      title: "Kite",
+      content:
+        "Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices.",
+      tryDemo: "/home",
+      learnMore: "/home",
+      playStore: "/home",
+      appleStore: "/home",
+    },
+  ];
   return (
     <>
-        <Hero/>
+      <Hero />
+      <LeftImage {...LeftImageContent[0]} />
+      <RightImage />
+      <Universe />
     </>
-  )
+  );
 }
