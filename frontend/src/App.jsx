230f34733995952ee61a2001367f37423a1bd530
Bhinsar <bhinsarjagdish2003@gmail.com>
2025-04-27 22:09:20 +0530

    Add SupportPage and related components: Hero and CreateTicket. Update routing in App.jsx to include SupportPage for enhanced user support experience.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import PageNoFound from "./page/Page-not-found/PageNoFound";
 import AboutPage from "./page/about/AboutPage";
 import ProductPage from "./page/products/ProductPage";
 import PricingPage from "./page/pricing/PricingPage";
-
+import SupportPage from "./page/support/SupportPage"; 
 function App() {
   return (
     <Router>
@@ -16,6 +16,7 @@ function App() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/product" element={<ProductPage />} />
           <Route path="/price" element={<PricingPage />} />
+          <Route path="/support" element={<SupportPage />} />
           <Route path="*" element={<PageNoFound />} />
         </Route>
       </Routes>
