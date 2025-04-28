2536a6e81d778740ca5b224be613ca9664c0c0cf
Bhinsar <bhinsarjagdish2003@gmail.com>
2025-04-28 14:51:57 +0530

    Update dependencies in package.json and package-lock.json; enhance Positions and WatchList components with dynamic data rendering and improved user interaction features.

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { positions } from "../data/data";
 
 const Positions = () => {
+
   return (
     <>
-      <h3 className="title">Positions (2)</h3>
+      <h3 className="title">Positions ({positions.length})</h3>
 
       <div className="order-table">
         <table>
@@ -16,6 +18,27 @@ const Positions = () => {
             <th>P&L</th>
             <th>Chg.</th>
           </tr>
+          {positions.map((positions, index) => {
+
+            const currentValue = positions.qty * positions.price;
+            const isProfit = currentValue- positions.avg * positions.qty >= 0.0;
+            const profClass = isProfit ? "profit" : "loss";  
+            const dayClass = positions.isLoss ? "loss" : "profit";
+
+            return (
+              <tr key={index} >
+                <td>{positions.product}</td>
+                <td>{positions.name}</td>
+                <td>{positions.qty.toFixed(2)}</td>
+                <td>{positions.avg.toFixed(2)}</td>
+                <td>{positions.price.toFixed(2)}</td>
+                <td className={profClass}>{(currentValue - positions.avg * positions.qty).toFixed(2)}</td>
+                <td className={dayClass}>{positions.day}</td>
+              </tr>
+
+            )
+
+          })}
         </table>
       </div>
     </>
--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,4 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { Tooltip, Grow } from "@mui/material";
+import { watchList } from "../data/data";
+import {
+  BarChartOutlined,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  MoreHoriz,
+} from "@mui/icons-material";
 
 const WatchList = () => {
   return (
@@ -11,12 +19,86 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchList.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchList.map((item, index) => {
+          return <WatchListItem key={index} item={item} />;
+        })}
+      </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+const WatchListItem = ({ item }) => {
+  const [showWatchlist, setShowWatchlist] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setShowWatchlist(true);
+  };
+
+  const handleMouseLeave = (e) => {
+    setShowWatchlist(false);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={item.isDown ? "down" : "up"}>{item.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{item.percent}</span>
+          {item.isDown ? (
+            <KeyboardArrowDown className="down" />
+          ) : (
+            <KeyboardArrowUp className="up" />
+          )}
+          <span className="price">{item.price}</span>
+        </div>
+      </div>
+      {showWatchlist && <WatchListActions uid={item.index} />}
+    </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  return (
+    <span className="actions">
+      <spam>
+        <Tooltip
+          title="Buy (B)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="buy">Buy</button>
+        </Tooltip>
+        <Tooltip
+          title="Sell (S)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="sell">Sell</button>
+        </Tooltip>
+        <Tooltip
+          title="Analytics (A)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="action">
+            <BarChartOutlined className="icon" />
+          </button>
+        </Tooltip>
+        <Tooltip title="More " placement="top" arrow TransitionComponent={Grow}>
+          <button className="action">
+            <MoreHoriz className="icon" />
+          </button>
+        </Tooltip>
+      </spam>
+    </span>
+  );
+};
